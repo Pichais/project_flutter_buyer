e557b955e5fdf053d5c3312d5082e497ffa7fd28
Bank <yougvb@gmail.com>
2022-05-13 23:08:17 +0700

    first commit

---
+++ routes/route.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+
+final Map<String, WidgetBuilder> map = {
+  // '/authen': (BuildContext context) => Authen(),
+  // '/createAccount': (BuildContext context) => Create(),
+  // '/myservice': (BuildContext context) => Myservice(),
+  // '/Category': (BuildContext context) => Category(),
+};
